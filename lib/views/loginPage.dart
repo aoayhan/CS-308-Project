077c76206a50f7ae2ebf5a045ed4af0a553afc5b
yslbsirem <75902455+yslbsirem@users.noreply.github.com>
2023-11-08 23:10:34 +0300

    loginPage.dart
    loginPage.dart

--- lib/views/loginPage.dart
+++ lib/views/loginPage.dart
@@ -2,9 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:supotify/views/signupPage.dart';
 import 'package:supotify/views/homePage.dart';
 
-
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  const LoginPage({Key? key});
 
   @override
   State<LoginPage> createState() => _LoginPageState();
@@ -12,6 +11,13 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   bool _rememberMe = false;
+  String? _errorText;
+  final String expectedUsername = 'your_username';
+  final String expectedPassword = 'your_password';
+
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,69 +33,81 @@ class _LoginPageState extends State<LoginPage> {
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 20),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
               child: TextField(
-                decoration: InputDecoration(
+                controller: usernameController,
+                decoration: const InputDecoration(
                   labelText: 'Username',
                   border: OutlineInputBorder(),
                 ),
               ),
             ),
             const SizedBox(height: 16),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
               child: TextField(
-                decoration: InputDecoration(
+                controller: passwordController,
+                decoration: const InputDecoration(
                   labelText: 'Password',
                   border: OutlineInputBorder(),
                 ),
-                obscureText: true, // Hide the password input
+                obscureText: true,
               ),
             ),
             const SizedBox(height: 24),
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                
-                Text("Remember me",),
-                Checkbox(value:  _rememberMe,
-                 onChanged: (bool? value){
-                  setState(() {
-                    _rememberMe = value ?? false;
-                  });
-                  
-                 },
-                 activeColor: Colors.greenAccent,),
-                 SizedBox(width: 20,),
+                const Text("Remember me"),
+                Checkbox(
+                  value: _rememberMe,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      _rememberMe = value ?? false;
+                    });
+                  },
+                  activeColor: Colors.greenAccent,
+                ),
+                const SizedBox(width: 20),
               ],
-              
             ),
-           
             ElevatedButton(
               onPressed: () {
-               Navigator.push(context, 
-               MaterialPageRoute(builder: (context) => const homePage())); // Add your login logic here
+                if (usernameController.text == expectedUsername &&
+                    passwordController.text == expectedPassword) {
+                  // Successful login, navigate to the home page
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const homePage()),
+                  );
+                } else {
+                  // Incorrect login, show an error message
+                  setState(() {
+                    _errorText =
+                        "The username or password you entered is incorrect.";
+                  });
+                }
               },
               child: const Text('Log In'),
             ),
             const SizedBox(height: 8),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-  children: [
-    Text("Don't have an account?",),
-     const SizedBox(width: 7,),
-    ElevatedButton(
-      onPressed: () {
-        Navigator.push(context, 
-        MaterialPageRoute(builder: (context) => const signupPage())); // Add your login logic here
-      },
-      child: const Text('Sign Up'),
-      
-    ),
-    
-  ],
-),
+            Text(_errorText ?? "", style: const TextStyle(color: Colors.red)),
+            const Text("Don't have an account?"),
+            const SizedBox(
+              width: 7,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const signupPage(),
+                  ),
+                );
+              },
+              child: const Text('Sign Up'),
+            ),
           ],
         ),
       ),
