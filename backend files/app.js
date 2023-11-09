92a395e6e55427bff15edd3768987371615cbf99
aoayhan <aoayhan@gmail.com>
2023-11-09 10:00:23 +0300

    Added backend files
    Co-Authored-By: Berkebariscan <98551100+Berkebariscan@users.noreply.github.com>

---
+++ backend files/app.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js';
+import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDdOI7OFmwajeufUvbp7I0_QbOKLK0whr4",
+    authDomain: "cs308fire.firebaseapp.com",
+    databaseURL: "https://cs308fire-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "cs308fire",
+    storageBucket: "cs308fire.appspot.com",
+    messagingSenderId: "351363273489",
+    appId: "1:351363273489:web:0d0d6bd36e77b4c28772b0",
+    measurementId: "G-0LVPP6YBVR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export { app };
